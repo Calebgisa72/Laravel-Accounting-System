eb1e809f69265cf11b6e38e308a0035a1542b40d
Gisa M. Caleb Pacifique <gisacaleb72@gmail.com>
2025-05-14 20:39:26 +0200

    Initial commit (removed sensitive data)

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    public function run(): void
+    {
+        User::factory()->create([
+            'name' => 'Super Admin',
+            'email' => 'superadmin@gmail.com',
+            'password'=> Hash::make('admin123'),
+            'role' => 'superadmin',
+            'provider' => 'simple'
+        ]);
+    }
+}
